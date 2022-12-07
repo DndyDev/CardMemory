9f27188f7b6b81bd0230224db2add68b5969267a
DandyDev <tikhomirov2000@inbox.ru>
2022-12-07 14:46:54 +0300

    update SceneController, add spawn card grid hardcode

--- Assets/Scripts/MemoryCard.cs
+++ Assets/Scripts/MemoryCard.cs
@@ -5,6 +5,19 @@ using UnityEngine;
 public class MemoryCard : MonoBehaviour
 {
     [SerializeField] private GameObject cardBack;
+    [SerializeField] private SceneController controller;
+
+    private int _id;
+    public int id
+    {
+        get { return _id; }
+    }
+
+    public void setCard(int id, Sprite image)
+    {
+        _id = id;
+        GetComponent<SpriteRenderer>().sprite = image; 
+    }
 
     private void OnMouseDown()
     {
--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class SceneController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public const int gridRows = 2;
+    public const int gridColumns = 4;
+    public const float offsetX = 2f;
+    public const float offsetY = 2.5f;
+
+    [SerializeField] private MemoryCard originalCard;
+    [SerializeField] private Sprite[] images;
+
+
+    private void Start()
     {
-        
+        Vector3 startPostion = originalCard.transform.position;
+
+        for (int i = 0; i < gridColumns; i++)
+        {
+            for (int j = 0; j < gridRows; j++)
+            {
+                MemoryCard card;
+                if (i == 0 && j == 0)
+                {
+                    card = originalCard;
+                }
+                else
+                {
+                    card = Instantiate(originalCard) as MemoryCard;
+                }
+                int id = Random.Range(0, images.Length);
+                originalCard.setCard(id, images[id]);
+
+                float positionX = (offsetX * i) + startPostion.x;
+                float positionY = -(offsetY * j) + startPostion.y;
+                originalCard.transform.position = new Vector3(positionX, positionY, startPostion.z);
+            }
+
+        }
     }
 }
